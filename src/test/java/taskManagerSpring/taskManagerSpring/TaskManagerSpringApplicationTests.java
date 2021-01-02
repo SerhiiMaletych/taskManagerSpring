2bbde2c5a0cbe25c9cca555b6bfd6756cf788b1d
SerhiiMaletych <maitrewop@gmail.com>
2021-01-02 15:54:26 +0200

    Initial commit

---
+++ src/test/java/taskManagerSpring/taskManagerSpring/TaskManagerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package taskManagerSpring.taskManagerSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
