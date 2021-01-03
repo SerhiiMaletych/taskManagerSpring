f803ecd8676fbe6782a12b3bc824ffde15a2c987
SerhiiMaletych <maitrewop@gmail.com>
2021-01-03 11:07:04 +0200

    07 - add 'expired' field and basic implementation

---
+++ src/main/java/taskManagerSpring/taskManagerSpring/model/Expired.java
@@ -0,0 +1,5 @@
+package taskManagerSpring.taskManagerSpring.model;
+
+public enum Expired {
+    HOURS_6, HOURS_12, ONE_DAY, TWO_DAYS, ONE_WEEK,ONE_MONTH
+}
--- src/main/java/taskManagerSpring/taskManagerSpring/model/Task.java
+++ src/main/java/taskManagerSpring/taskManagerSpring/model/Task.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import javax.validation.constraints.Size;
 import javax.persistence.*;
 
@@ -12,20 +13,28 @@ import javax.persistence.*;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name="task")
+@Table(name = "task")
 public class Task {
-
 
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Size(min=2, max=200)
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Size(min = 2, max = 200)
     @Column(name = "text", nullable = false)
     private String text;
+
     @Column(name = "status", nullable = false)
     @Enumerated(value = EnumType.STRING)
     private Status status;
+
+    @Column(name = "expired", nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private Expired expired;
 
 
     public Long getId() {
@@ -34,6 +43,14 @@ public class Task {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getText() {
@@ -50,5 +67,14 @@ public class Task {
 
     public void setStatus(Status status) {
         this.status = status;
+    }
+
+
+    public Expired getExpired() {
+        return expired;
+    }
+
+    public void setExpired(Expired expired) {
+        this.expired = expired;
     }
 }
