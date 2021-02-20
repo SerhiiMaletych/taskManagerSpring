2c47d303f286b19fdfb3be3fa4b1180bbc76206b
SerhiiMaletych <maitrewop@gmail.com>
2021-02-20 22:28:06 +0200

    40 - add lombok to a project

--- src/main/java/taskManagerSpring/taskManagerSpring/model/Block.java
+++ src/main/java/taskManagerSpring/taskManagerSpring/model/Block.java
@@ -1,49 +1,34 @@
 package taskManagerSpring.taskManagerSpring.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 
+@Builder
+@Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @DynamicUpdate
 @Table(name = "block")
 public class Block {
 
-    public Block() {
 
-    }
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @javax.persistence.Column(name = "name", nullable = false)
+    private String name;
 
     public Block(String name) {
         this.name = name;
     }
 
-    @javax.persistence.Column(name = "name", nullable = false)
-    private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Block(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
 
 }
--- src/main/java/taskManagerSpring/taskManagerSpring/model/Task.java
+++ src/main/java/taskManagerSpring/taskManagerSpring/model/Task.java
@@ -1,27 +1,33 @@
 package taskManagerSpring.taskManagerSpring.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 import javax.validation.constraints.Size;
 
 
-
+@Builder
+@Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @DynamicUpdate
 @Table(name = "task")
 public class Task {
 
-    public Task() {
-    }
-    public Task(Long id, String title, @Size(min = 2, max = 200) String text, Status status, Expired expired,String date) {
+
+    public Task(Long id, String title, @Size(min = 2, max = 200) String text, Status status, Expired expired, String date) {
         this.id = id;
         this.title = title;
         this.text = text;
         this.status = status;
         this.expired = expired;
-        this.date=date;
+        this.date = date;
 
     }
 
@@ -55,63 +61,6 @@ public class Task {
 
     @Column(name = "expireddate")
     private String expiredDate;
-
-
-    public String getExpiredDate() {
-        return expiredDate;
-    }
-
-    public void setExpiredDate(String expiredDate) {
-        this.expiredDate = expiredDate;
-    }
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-
-    public Expired getExpired() {
-        return expired;
-    }
-
-    public void setExpired(Expired expired) {
-        this.expired = expired;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
 
 
 
